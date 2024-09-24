f57798a803a517a90995f02b67fdcc45638c389d
Olha Teplova <olitieplove@gmail.com>
2024-09-24 14:01:43 +0300

    Clear gh-pages branch

--- tests/pages/search.page.js
+++
@@ -1,9 +0,0 @@
-export class SearchPage {
-  constructor(page) {
-    this.page = page;
-
-    this.title = page.locator("h2").nth(0);
-
-    this.searchInput = page.locator("#search-input");
-  }
-}
